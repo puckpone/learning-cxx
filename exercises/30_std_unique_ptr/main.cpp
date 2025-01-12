0bc89f97e43c0a69de771d2a7231d1ba7aff07a4
puck <1093308061@qq.com>
2025-01-12 16:46:18 +0800

    modify 30

--- exercises/30_std_unique_ptr/main.cpp
+++ exercises/30_std_unique_ptr/main.cpp
@@ -7,17 +7,19 @@
 // READ: `std::unique_ptr` <https://zh.cppreference.com/w/cpp/memory/unique_ptr>
 
 std::vector<std::string> RECORDS;
-
+int FUCK=1;
 class Resource {
     std::string _records;
-
+    int fuck;
 public:
     void record(char record) {
         _records.push_back(record);
+        this->fuck = FUCK++;
     }
 
     ~Resource() {
         RECORDS.push_back(_records);
+        std::cout << this->fuck << " has been destoryed" << std::endl;
     }
 };
 
@@ -52,9 +54,9 @@ int main(int argc, char **argv) {
     std::vector<const char *> answers[]{
         {"fd"},
         // TODO: 分析 problems[1] 中资源的生命周期，将记录填入 `std::vector`
-        // NOTICE: 此题结果依赖对象析构逻辑，平台相关，提交时以 CI 实际运行平台为准
-        {"ffr", "d", "f"},
-        {"r", "r", "d", "r", "d", "d"}
+        // NOTICE: 此题结果依赖对象析构逻辑，平台相关，提交时以 CI 实际运行平台（gcc）为准
+        {"ffr", "d"},
+        {"r", "d", "d"}
     };
 
     // ---- 不要修改以下代码 ----
